dd87e760455b257193e9da9986b7fa2e76b43340
sebastian2540 <sebastian.villada.jaramillo@gmail.com>
2024-02-27 20:50:17 -0500

    firts commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+function App() {
+
+  return (
+    <section className="contenido">
+      <header>
+        <img src="public/vite.svg" alt="" />
+        <nav>
+          <a href="">Servicios</a>
+          <a href="">Productos</a>
+          <a href="">Blog</a>
+          <a href="">Contacto</a>
+        </nav>
+      </header>
+
+      <aside>
+
+      </aside>
+
+      <main>
+        <section>
+
+        </section>
+
+        <section>
+
+        </section>
+      </main>
+    </section>
+  )
+}
+
+export default App
